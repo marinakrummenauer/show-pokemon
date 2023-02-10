a3e453528ff97bd0c14e5ce852e95d77f4d8e67e
marinakrummenauer <marinakrummenauer92@gmail.com>
2023-02-10 02:14:16 -0300

    instalação de alguns itens pendentes, e  ajuste do layout responsivo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
-    <form @submit.prevent="searchPokemon">
-      <input type="text" v-model="pokemonName" placeholder="Pesquise aqui seu Pokémon">
-      <button type="submit">Buscar</button>
-    </form>
-    <ul v-if="pokemon">
-      <li v-for="pokemon in pokemons"  v-bind:key="pokemon.id">{{ pokemon.name }}</li>
-    </ul>
+  <div class="pokemon">
+    <h1 class="title">Localize seu</h1> <img class="logo-pokemon align-center" src="@/assets/pokemon-logo.png">
+    <div class="form-container">
+      <form @submit.prevent="searchPokemon">
+        <input type="text" v-model="pokemonName" placeholder="Pesquise seu Pokémon">
+        <button type="submit">Buscar</button>
+      </form>
+      <div v-if="errorMessage" class="alert alert-danger" role="alert">
+        {{ errorMessage }}
+      </div>
+      <ul v-if="pokemons.length">
+        <li v-for="pokemon in pokemons"  v-bind:key="pokemon.id">
+          <h2>{{ pokemon.name }}</h2>
+          <img :src="pokemon.sprites.front_default" alt="Pokemon sprite">
+          <ul>
+            <li><strong>HP:</strong> {{ pokemon.stats[5].base_stat }}</li>
+            <li><strong>Attack:</strong> {{ pokemon.stats[4].base_stat }}</li>
+            <li><strong>Defense:</strong> {{ pokemon.stats[3].base_stat }}</li>
+            <li><strong>Special Attack:</strong> {{ pokemon.stats[2].base_stat }}</li>
+            <li><strong>Special Defense:</strong> {{ pokemon.stats[1].base_stat }}</li>
+            <li><strong>Speed:</strong> {{ pokemon.stats[0].base_stat }}</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -15,14 +32,69 @@ export default {
   data() {
     return {
       pokemonName: '',
-      pokemons: []
+      pokemons: [],
+      errorMessage: ''
     }
   },
   methods: {
     async searchPokemon() {
-      const response = await this.$http.get(`https://pokeapi.co/api/v2/pokemon/${this.pokemonName}`);
-      this.pokemons = response.data;
+      try {
+        const response = await this.$http.get(`https://pokeapi.co/api/v2/pokemon/${this.pokemonName}`);
+        this.pokemons = response.data;
+        this.errorMessage = '';
+      } catch (error) {
+        this.errorMessage = 'Desculpe, este Pokémon não foi encontrado. Informe novamente no campo de pesquisa';
+      }
     }
   }
 }
 </script>
+<style lang="scss">
+
+.pokemon{
+  .form-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 200px;
+  }
+  .title{
+    text-align: center;
+    padding-top:50px;
+  }
+  input{
+    border: 2px solid #f1c204;
+    border-radius:2px;
+    font-size:18px;
+    height:32px;
+  }
+  button{
+    margin-left:8px;
+    background-color:#2e63aa;
+    color: #f1c204;
+    border: 2px solid #2e63aa;
+    border-radius:10px;
+    font-size:24px;
+  }
+  img{
+    justify-content: center  !important;
+    align-content: center !important;
+    display: flex !important;
+    right:0;
+  }
+  @media (min-width: 320px ) and (max-width: 991px) {
+    button{
+      margin-left:4px;
+      font-size:24px;
+    }
+  }
+  @media (min-width: 1024px ) {
+    button{
+      margin-left:8px;
+      font-size:18px;
+      width: 150px;
+      height: 32px;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import VueRouter from 'vue-router';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.prototype.$http = axios;
 
